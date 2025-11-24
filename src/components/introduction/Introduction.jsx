bac8ba7a3ada0f8e1159b616b7cb8a408fc36585
Leonel Moreno <48166747+leonelmorenoa@users.noreply.github.com>
2025-11-23 21:57:49 -0400

    Update Introduction.jsx

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -2,6 +2,7 @@ import person from "../../assets/images/person.png";
 import "./introduction.css";
 import InformationSummary from "./InformationSummary";
 import { Link } from "react-scroll";
+
 // Information summary data
 const informationSummaryData = [
   {
@@ -42,26 +43,36 @@ const Introduction = () => {
 
           {/* 🔥 Puedes personalizar este párrafo luego */}
           <p className="text-xs xxs:text-lg lg:text-[18px] my-6">
-          I turn raw, scattered data into<span className="bg-[#1E96FC33] text-[#000080] px-2 py-0.5 rounded-lg"> Data Systems</span>
-          that empower <span className="bg-[#16ed48] text-[#000080] px-2 py-0.5 rounded-lg">better decisions </span> — helping organizations act faster, <span className="bg-[#FFBF00] text-[#000080] px-2 py-0.5 rounded-lg"> reduce uncertainty</span>, 
-          and unlock measurable business outcomes.
+            I turn raw, scattered data into
+            <span className="bg-[#1E96FC33] text-[#000080] px-2 py-0.5 rounded-lg"> Data Systems</span>
+            that empower <span className="bg-[#16ed48] text-[#000080] px-2 py-0.5 rounded-lg">better decisions </span> — helping organizations act faster,
+            <span className="bg-[#FFBF00] text-[#000080] px-2 py-0.5 rounded-lg"> reduce uncertainty</span>, 
+            and unlock measurable business outcomes.
           </p>
 
           <p className="text-center lg:text-start">
-<Link
-  to="contact"
-  smooth={true}
-  duration={900}
-  offset={-140} 
-  className="btn-primary btn btn-xs xxs:btn-lg text-white"
->
-  Get in Touch
-</Link>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={900}
+              offset={-140}
+              className="btn-primary btn btn-xs xxs:btn-lg text-white"
+            >
+              Get in Touch
+            </Link>
+          </p>
+        </div>
+
+        <div className="mx-auto lg:mx-0 relative">
+          <div className="grid max-xxs:grid-flow-col grid-cols-3 w-fit mt-10 gap-1">
+            {informationSummaryData.map((item) => (
+              <InformationSummary key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
       </div>
 
-      <div
-        className="max-w-134 w-full h-full max-lg:mx-auto aspect-[536/636] relative"
-      >
+      <div className="max-w-134 w-full h-full max-lg:mx-auto aspect-[536/636] relative">
         <img
           className="shadow-2xl shadow-gray-200 w-full h-full absolute bottom-0 object-cover bg-white rounded-3xl"
           src={person}
@@ -72,7 +83,4 @@ const Introduction = () => {
   );
 };
 
-
 export default Introduction;
-
-
